0007a0c8ff616a8670137c45a014f3294684001e
LCJ-MinYa <1049468118@qq.com>
2018-08-03 15:39:50 +0800

    四大领域

--- whydahWeb/dist/js/fourAreas.js
+++ whydahWeb/dist/js/fourAreas.js
@@ -1,1 +1,44 @@
-fourAreas.js
+/*
+ * ===========================
+ *
+ * 四大领域 => fourAreas
+ * @author   : LiChaoJun
+ * @datetime : 2018/08/03
+ *
+ * ===========================
+ */
+(function() {
+    var clickFunc = function() {
+        $(".config-list ul li").click(function() {
+            if (!$(this).hasClass("active")) {
+                $(this).addClass("active").siblings().removeClass("active");
+                $(this).parent().parent().find("ol li").eq($(this).index()).addClass("active").siblings().removeClass("active");
+            }
+        })
+    }
+
+    var bannerSwiperInit = function() {
+        var bannerSwiper = new Swiper('#js_banner', {
+            loop: true,
+            pagination: '.pagination',
+            paginationClickable: true,
+            preventLinks: false,
+            autoplay: 5000,
+        })
+
+        $('.arrow-left').on('click', function(e) {
+            e.preventDefault()
+            bannerSwiper.swipePrev()
+        })
+        $('.arrow-right').on('click', function(e) {
+            e.preventDefault()
+            bannerSwiper.swipeNext()
+        })
+    }
+
+    /*------ 执行方法 ------*/
+    $(function() {
+        clickFunc();
+        bannerSwiperInit();
+    });
+}());
